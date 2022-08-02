120902d87611c4f23cd936413e0418ef6b896ca3
ManuelUrquina <mfurquina2004@gmail.com>
2022-08-02 16:41:39 -0500

    Added a new page called Productos que soportamos de zepben and add a card style to the products

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,102 +6,100 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Enphoria',
-  tagline: '',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: ' ',
+    title: 'Enphoria',
+    tagline: '',
+    url: 'https://your-docusaurus-test-site.com',
+    baseUrl: '/',
+    onBrokenLinks: 'throw',
+    onBrokenMarkdownLinks: 'warn',
+    favicon: ' ',
 
-  // Even if you don't use internalization, you can use this field to set useful
-  // metadata like html lang. For example, if your site is Chinese, you may want
-  // to replace "en" with "zh-Hans".
-  i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
-  },
+    // Even if you don't use internalization, you can use this field to set useful
+    // metadata like html lang. For example, if your site is Chinese, you may want
+    // to replace "en" with "zh-Hans".
+    i18n: {
+        defaultLocale: 'en',
+        locales: ['en'],
+    },
 
-  presets: [
-    [
-      'classic',
-      /** @type {import('@docusaurus/preset-classic').Options} */
-      ({
-        docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-        },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-        },
-        theme: {
-          customCss: require.resolve('./src/css/custom.css'),
-        },
-      }),
+    presets: [
+        [
+            'classic',
+            /** @type {import('@docusaurus/preset-classic').Options} */
+            ({
+                docs: {
+                    sidebarPath: require.resolve('./sidebars.js'),
+                    // Please change this to your repo.
+                },
+                blog: {
+                    showReadingTime: true,
+                    // Please change this to your repo.
+                },
+                theme: {
+                    customCss: require.resolve('./src/css/custom.css'),
+                },
+            }),
+        ],
     ],
-  ],
 
-  themeConfig:
+    themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-    ({
-      navbar: {
-        title: 'Enphoria',
-        logo: {
-          alt: 'Enphoria',
-          src: 'img/Enphoria.png',
-        },
-        items: [
-          {
-           type: 'doc',
-            docId: 'Manual de Usuario',
-            position: 'left',
-            label: 'Fault Location',
-          },
+        ({
+            navbar: {
+                title: 'Enphoria',
+                logo: {
+                    alt: 'Enphoria',
+                    src: 'img/Enphoria.png',
+                },
+                items: [
+                    {
+                        type: 'doc',
+                        docId: 'Manual de Usuario',
+                        position: 'left',
+                        label: 'Productos Enphoria',
+                    },
 
-          /*{to: '/blog', label: 'Blog', position: 'left'},/**/
+                    {
+                        type: 'doc',
+                        docId: 'Zepben',
+                        position: 'left',
+                        label: 'Productos que soportamos de Zepben',
 
-          {
-            href: 'https://github.com/enphoria/ms-fault-location',
-            label: 'GitHub',
-            position: 'right',
-          },
-        ],
-      },
-      footer: {
-        style: 'dark',
-        links: [
-          {
-            title: 'Documentos en Github',
-            items: [
-              {
-                label: 'Manual de Usuario',
-                to: '/docs/Manual de Usuario',
-              },
+                    },
 
-              {
-                label: 'Manual tecnico',
-                to: '/docs/Manual Tecnico',
-              },           
-        ],
-      },
-          {
-            title: 'More',
-            items: [
-              {
-                label: 'GitHub',
-                href: 'https://github.com/enphoria/',
-              },
-            ],
-          },
-        ],
-        copyright: `Enphoria © ${new Date().getFullYear()}.`,
-      },
-      prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
-      },
-    }),
+                    /*{to: '/blog', label: 'Blog', position: 'left'},/**/
+                ],
+            },
+            footer: {
+                style: 'light',
+                links: [
+                    
+                    {
+                        title: 'Enphoria',
+                        items: [
+                            {
+                                label: 'GitHub',
+                                href: 'https://github.com/enphoria/',
+                            },
+                        ],
+                    },
+
+                    {
+                        title: 'Zepben',
+                        items: [
+                            {
+                                label: 'GitHub',
+                                href: 'https://github.com/zepben',
+                            },
+                        ],
+                    },                
+                ],
+                copyright: `Enphoria © ${new Date().getFullYear()}.`,
+            },
+            prism: {
+                darkTheme: darkCodeTheme,
+            },
+        }),
 };
 
 module.exports = config;
